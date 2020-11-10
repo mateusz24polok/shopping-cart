5b8f6ea52f3e4759011d43145a1ef1d4d107b42c
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-10 21:07:40 +0100

    basic structure of project and MainPage template have been created

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledWrapper } from "./styled";
+
+const MainPage = () => {
+    return (
+        <StyledWrapper>Main Page</StyledWrapper>
+    );
+};
+
+export default MainPage;
---
+++ src/pages/MainPage/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+    max-width: 1000px;
+    margin: 50px auto 0;
+`; 
