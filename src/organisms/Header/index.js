d37ee7050d1e8678c126f9188d9c95f05a5c7cdb
Mateusz Polok <mateusz24polok@gmail.com>
2020-11-11 09:28:51 +0100

    open modal functionality has been added to header purchase button

--- src/organisms/Header/index.js
+++ src/organisms/Header/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { StyledWrapper, Title } from "./styled";
 import PrimaryButton from "../../components/PrimaryButton";
+import { openModal } from "../../slices/ShoppingCartSlice";
 
 const Header = () => {
+
+    //Take dispatch from react-redux
+
+    const dispatch = useDispatch();
+
+    // Function for handle Modal Open - only open the Modal, clear shopping cart and summarize section will be after modal close
+
+    const handleModalOpen = () => {
+        dispatch(openModal());
+    };
+
     return (
         <StyledWrapper>
             <Title>Shopping Cart</Title>
-            <PrimaryButton>Proceed to checkout</PrimaryButton>
+            <PrimaryButton onClick={handleModalOpen}>Proceed to checkout</PrimaryButton>
         </StyledWrapper>
     );
 };
